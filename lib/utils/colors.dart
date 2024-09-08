394aa79c8055984d7c3c51969771764ce6ad83bf
Noureldin17 <noureldinn.nm@gmail.com>
2024-09-08 10:50:05 +0300

    added app utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryDark = Color(0xFF08081D);
+const primaryPurple = Color(0xFF7D4192);
+const primaryBlue = Color(0xFF369BFF);
+const primaryGrey = Color(0xFF6A707C);
+const primarySilver = Color(0xFF8391A1);
+const shimmerBase = Color.fromARGB(255, 17, 17, 40);
+const shimmerLoad = Color.fromARGB(255, 31, 31, 60);
